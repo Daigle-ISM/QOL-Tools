f529ac0af034023689d6e9a9640edff7dd2180fd
Jonathan Daigle <Jonathan.Daigle@Kyndryl.com>
2023-07-07 13:49:46 -0700

    UWP, not WPF

--- PowerShellScripts/UpdateRDPFiles.ps1
+++ PowerShellScripts/UpdateRDPFiles.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
     Adds custom configurations to the WPF Remote Desktop App
 .DESCRIPTION
-    The WPF Remote Desktop app for Windows does not expose many of the options that people are used to for RDP. This script locates the .rdp files used by this tool and updates them with the required configurations
+    The UWP Remote Desktop app for Windows does not expose many of the options that people are used to for RDP. This script locates the .rdp files used by this tool and updates them with the required configurations
 .NOTES
     The .rdp files this script modifies get over-written frequently, it is necessary to run this script frequently (at logon or screen unlock is a good option)
 #>
