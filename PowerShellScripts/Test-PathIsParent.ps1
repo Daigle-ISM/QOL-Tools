daa8d967bdecb7deec72f2f22988af7d19a9a709
Jonathan Daigle <Jonathan.Daigle@Kyndryl.com>
2023-07-18 14:15:09 -0700

    Add support for pipelining, update help, add verbose and debug

--- PowerShellScripts/Test-PathIsParent.ps1
+++ PowerShellScripts/Test-PathIsParent.ps1
@@ -6,51 +6,62 @@
 .NOTES
     For best results use the full paths for the parent and child.
 
+    Testing Windows paths on a non-windows system does work but the paths it ends up comparing are relative to your home folder which may cause unexpected behaviour.
+    e.g.: C:\Test becomes /home/user/C:/Test.
+
+    This does with with URLS, smb:// and https:// urls, for example.
+
     This function is case-insensitive on Windows by default, but is case-sensitive on other platforms
 .EXAMPLE
-    Test-MyTestFunction -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    Test-PathIsParent -PotentialParent C:\Test -PotentialChild C:\Test\Child
+    Returns True, as C:\Test is a parent of C:\Test\Child
+.EXAMPLE
+    Test-PathIsParent -PotentialParent C:\Test -PotentialChild C:\TestChild
+    Returns False, as C:\Test is not a parent of C:\TestChild
 .PARAMETER PotentialParent
-    The potential parent directory. This must be a directory. Ideally the full path should be provided.
+    The potential parent directory. This must be a directory, or the script will always return $false.
 .PARAMETER PotentialChild
-    The potential child item. This can be a file or directory. Ideally the full path should be provided.
-.PARAMETER DirectorySeparatorChar
-    The directory separator character to use. If not specified, the function will attempt to determine the correct character to use based on the childpath.
+    The potential child item. This can be a file or directory.
 #>
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
     [System.IO.DirectoryInfo]$PotentialParent,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
     [System.IO.DirectoryInfo]$PotentialChild,
-    [char]
-    $DirectorySeparatorChar = 0,
-    [bool]
     # Default to case insensitive on Windows. Non-core is only available on Windows, and doesn't have the $IsWindows variable
+    [bool]
     $CaseSensitive = $PSEdition -eq 'Core' -and !($IsWindows)
 )
-# If the character has not been provided, chooses the alternate directory character only if it is found in the child path, but the primary directory character is not
-if ($DirectorySeparatorChar -eq 0) {
-    if (-not $PotentialParent.FullName.Contains([System.IO.Path]::DirectorySeparatorChar) -and $PotentialChild.FullName.Contains([System.IO.Path]::AltDirectorySeparatorChar)) {
-        $DirectorySeparatorChar = [System.IO.Path]::AltDirectorySeparatorChar;
+
+begin {
+    if (!($PotentialParent.Exists)) {
+        Write-Warning "Parent path '$($PotentialParent.FullName)' is not a folder or does not exist."
     }
-    else {
-        $DirectorySeparatorChar = [System.IO.Path]::DirectorySeparatorChar;
-    }
+    Write-Debug "Directory Separator: $([System.IO.Path]::DirectorySeparatorChar)"
+    Write-Verbose "Case Sensitive: $CaseSensitive"
 }
-$ParentSegments = ($PotentialParent.FullName).TrimEnd($DirectorySeparatorChar).Split($DirectorySeparatorChar)
-$ChildSegments = ($PotentialChild.FullName).TrimEnd($DirectorySeparatorChar).Split($DirectorySeparatorChar)
-# If all the parent segments equal the child segments, the path is a match
-for ($i = 0; $i -lt $ParentSegments.Count; $i++) {
-    if ($CaseSensitive) {
-        if ($ParentSegments[$i] -cne $ChildSegments[$i]) {
-            return $false
+
+process {
+    Write-Verbose "Processing '$($PotentialChild.FullName)'"
+    # Using TrimEnd instead of TrimEndingDirectorySeparator() because the latter will not trim from a root directory
+    $ParentSegments = ($PotentialParent.FullName).TrimEnd([System.IO.Path]::DirectorySeparatorChar).Split([System.IO.Path]::DirectorySeparatorChar)
+    Write-Debug ("ParentSegments:`n" + ($ParentSegments | Format-Table | Out-String))
+    $ChildSegments = ($PotentialChild.FullName).TrimEnd([System.IO.Path]::DirectorySeparatorChar).Split([System.IO.Path]::DirectorySeparatorChar)
+    Write-Debug ("ChildSegments:`n" + ($ChildSegments | Format-Table | Out-String))
+
+    # If all the parent segments equal the corresponding child segments the path is a match
+    for ($i = 0; $i -lt $ParentSegments.Count; $i++) {
+        if ($CaseSensitive) {
+            if ($ParentSegments[$i] -cne $ChildSegments[$i]) {
+                return $false
+            }
+        }
+        else {
+            if ($ParentSegments[$i] -ine $ChildSegments[$i]) {
+                return $false
+            }
         }
     }
-    else {
-        if ($ParentSegments[$i] -ine $ChildSegments[$i]) {
-            return $false
-        }
-    }
+    return $true
 }
-return $true
